32fe0b9e3f16615e06e8a67df62e00e0ea853b59
Iraiore <yagoiraiore@gmail.com>
2024-03-31 15:24:00 -0400

    implentado a soma todal de bens

---
+++ AppInventario/AppInventario/Services/PropriedadeService.cs
@@ -0,0 +1,36 @@
+﻿using AppInventario.Contexto;
+using AppInventario.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AppInventario.Services
+{
+    public class PropriedadeService
+    {
+        private readonly ContextoBD _context;
+
+        public PropriedadeService(ContextoBD con)
+        {
+
+            _context = con;
+        }
+
+        public async Task Add(List<Propriedade> bens)
+        {
+            if (bens != null)
+            {
+                await _context.Propriedades.AddRangeAsync(bens);
+            }
+        }
+
+        public async Task Salvar()
+        {
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Propriedade>> Propriedades()
+        {
+            var p = await _context.Propriedades.Include(p => p.Pessoa).ToListAsync();
+            return p;
+        }
+    }
+}
